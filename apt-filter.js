381e11b4e249e74de78fb4f0d709b578bbdd8936
zakgarchad22 <zakgarchad@gmail.com>
2023-12-04 17:09:05 +0200

    complete handlebars project

--- apt-filter.js
+++ apt-filter.js
@@ -2,15 +2,17 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
-    let relevantApts = apartments.filter(a =>
-        a.address.toLowerCase().includes(address.toLowerCase() || "") &&
-        a.price >= (minPrice || 0) &&
-        a.price <= (maxPrice || a.price) &&
-        a.rooms >= (minRooms || 0) &&
-        a.rooms <= (maxRooms || a.rooms)
-    )
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking) {
+    
+    return apartments.filter(a =>
+        a.address.toLowerCase().includes(address.toLowerCase()) &&
+        a.price >= minPrice &&
+        a.price <= maxPrice &&
+        a.rooms >= minRooms &&
+        a.rooms <= maxRooms &&
+        a.immediate === immediate &&
+        a.parking === parking
 
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+
+    )
 }
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
+// let address = "P. Sherman 42 Wallabe Way, Sydney"
+// let minPrice = 2000
+// let maxPrice = 3000
+// let minRooms = 2
+// let maxRooms = 2
+// let immediate = false
+// let parking = true
+
 $("button").on("click", function () {
     let address = $("#addr-input").val()
     let minPrice = $("#min-p-input").val()
@@ -6,15 +14,22 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
-
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let parking = $("#park-y")
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate ,parking)
+    
     renderApts(relevantApts)
 })
 
+
+
 const renderApts = function (apartments) {
     $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
+    console.log(apartments)
+    // Your code goes here.
+    const source = $("#apartment-template").html()
+    const template = Handlebars.compile(source)
+    const newHTML = template( {apartment : apartments} )
+    $("#results").html(newHTML)
 }
 
 renderApts(apartments) //renders apartments when page loads
